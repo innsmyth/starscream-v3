8da13c55643311ad6e22a5b741a304532cf1ca66
Kallan Innsmyth <goolagooncreature@gmail.com>
2026-01-14 21:02:05 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "JetScreen",
+  title: "StarScream",
 };
 
 export default function RootLayout({
