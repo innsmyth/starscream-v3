3efe1fd9c5a3302c6e3b4df6c32cc06130f39091
innsmyth <goolagooncreature@gmail.com>
2026-03-01 14:48:07 -0600

    update to start script

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-export ME="kallan"
+user="starscream"
+
+if [[ ! id $user &>/dev/null ]]; then
+	sudo useradd -m $user
+	sudo usermod -aG sudo $user
+fi
+
+su - $user
 
 if [[ ! -f /starscream ]]; then
 	sudo mkdir /starscream/
-	sudo chown $ME:$ME /starscream/
-	sudo cp /home/$ME/starscream-v3/initial_setup.sh /starscream/
+	sudo chown $user:$user /starscream/
+	setuploc="$( cd "$( dirname "${BASSH_SOURCE[0]}" )" &> /dev/null && pwd )"
+	sudo cp $setuploc/initial_setup.sh /starscream/
 fi
 
 if [[ ! -f "/etc/rc.local" ]]; then
@@ -19,7 +27,7 @@ fi
 
 if [[ ! -f "/starscream/setup.log" ]]; then
 	sudo touch /starscream/setup.log
-	sudo chown $ME:$ME /starscream/setup.log
+	sudo chown $user:$user /starscream/setup.log
 	sudo echo "start" > /starscream/setup.log
 fi
 
@@ -58,8 +66,8 @@ case $(tail -n 1 /starscream/setup.log) in
 		sudo echo "piaware services enabled" > /starscream/setup.log
 		;;
 	"piaware services enabled")
-		cp /home/$ME/starscream-v3/.env.default /home/$ME/starscream-v3/.env.local
-		cd /home/$ME/starscream-v3/
+		cp /home/$user/starscream-v3/.env.default /home/$user/starscream-v3/.env.local
+		cd /home/$user/starscream-v3/
 		npm run build
 		npm start
 		sudo apt install firefox -y
