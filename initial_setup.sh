4bcdb8c89a48fecd2d4aa2ff6750011eb2d0dbdc
innsmyth <goolagooncreature@gmail.com>
2026-03-01 17:23:56 -0600

    setup updates

--- initial_setup.sh
+++ initial_setup.sh
@@ -2,18 +2,20 @@
 
 user="starscream"
 
-if [[ ! id $user &>/dev/null ]]; then
-	sudo useradd -m $user
-	sudo usermod -aG sudo $user
+if ! id $user &>/dev/null; then
+	sudo useradd -r -s /usr/sbin/nologin -M -c "Service Account for starscream" $user
 fi
-
-su - $user
 
 if [[ ! -f /starscream ]]; then
 	sudo mkdir /starscream/
 	sudo chown $user:$user /starscream/
 	setuploc="$( cd "$( dirname "${BASSH_SOURCE[0]}" )" &> /dev/null && pwd )"
 	sudo cp $setuploc/initial_setup.sh /starscream/
+	cd /starscream/
+	git clone https://github.com/innsmyth/starscream-v3.git
+	cp /starscream/starscream-v3/.env.default /starscream/starscream-v3/.env.local
+	sudo chown -R $user:$user /starscream/
+	
 fi
 
 if [[ ! -f "/etc/rc.local" ]]; then
@@ -28,14 +30,16 @@ fi
 if [[ ! -f "/starscream/setup.log" ]]; then
 	sudo touch /starscream/setup.log
 	sudo chown $user:$user /starscream/setup.log
-	sudo echo "start" > /starscream/setup.log
+	sudo bash -c 'echo "start" > /starscream/setup.log'
 fi
+
+#su - $user
 
 case $(tail -n 1 /starscream/setup.log) in
 	"start")
 		sudo apt update
 		sudo apt full-upgrade -y
-		sudo echo "initial updates" > /starscream/setup.log
+		sudo bash -c 'echo "initial updates" > /starscream/setup.log'
 		sudo reboot
 		;;
 	"initial updates")
@@ -52,7 +56,7 @@ case $(tail -n 1 /starscream/setup.log) in
 		sudo apt install dump978-fa -y
 		sudo apt install nodejs npm -y
 
-		sudo echo "piaware and dump installed" > /starscream/setup.log
+		sudo bash -c 'echo "piaware and dump installed" > /starscream/setup.log'
 		sudo reboot
 		;;
 	"piaware and dump installed")
@@ -63,16 +67,16 @@ case $(tail -n 1 /starscream/setup.log) in
 		sudo systemctl enable piaware
 		sudo systemctl restart piaware
 
-		sudo echo "piaware services enabled" > /starscream/setup.log
+		sudo bash -c 'echo "piaware services enabled" > /starscream/setup.log'
 		;;
 	"piaware services enabled")
-		cp /home/$user/starscream-v3/.env.default /home/$user/starscream-v3/.env.local
-		cd /home/$user/starscream-v3/
+		cd /starscream/starscream-v3
 		npm run build
 		npm start
 		sudo apt install firefox -y
 		firefox --kiosk http://localhost:3000
+		;;
 	*)
-		echo "this script is broken, delete setup.log at /var/run/setup.log"
+		sudo echo "this script is broken, delete setup.log at /var/run/setup.log"
 		;;
 esac
